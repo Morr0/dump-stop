9b4552f57e5e61b8d59dfe4e9a88a1da49754b41
Rami Hikmat <ramihikmat10@gmail.com>
2020-06-19 12:42:35 +1000

    Nice things but not enough

--- scripts/HTMLconstants.js
+++
@@ -1,9 +0,0 @@
-import FileItem from "../components/fileItem.js";
-
-// Holds the markup of the file
-const independentFileItem = "";
-
-// param:independent = does not have a child nor a parent
-function generateFile(independent, id, title){
-    
-}
